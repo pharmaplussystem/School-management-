3efac204b0ffc57c63395b9d802756f2ebecdf0b
pharmaplussystem <pharmaplussystem@gmail.com>
2026-09-17 04:28:51 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const SUPABASE_URL=""; const SUPABASE_ANON_KEY="";
+const SUPABASE_URL="https://fsholywcfksmposqusye.supabase.co/rest/v1/"; const SUPABASE_ANON_KEY="sb_publishable_rIXfUpyLfzCesaTP1E9I0Q_cmXbUv9l";
